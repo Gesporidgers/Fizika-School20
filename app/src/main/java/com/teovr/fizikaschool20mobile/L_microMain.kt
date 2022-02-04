edc1afcb54a11c61670b6e59f709e40b5f0f59ff
Gesporidgers <konkord302@gmail.com>
2022-02-04 11:41:29 +0300

    recyclerView upd

--- app/src/main/java/com/teovr/fizikaschool20mobile/L_microMain.kt
+++ app/src/main/java/com/teovr/fizikaschool20mobile/L_microMain.kt
@@ -2,22 +2,26 @@ package com.teovr.fizikaschool20mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.teovr.fizikaschool20mobile.databinding.ActivityLmicroMainBinding
 
 class L_microMain : AppCompatActivity() {
     lateinit var binding: ActivityLmicroMainBinding
+    private val adapter = BundleAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         binding = ActivityLmicroMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val DescP1 = listOf(
-            "Весы электронные",
-            "Измерительный цилиндр ",
-        )
-        val DeskP2 = listOf(
-            "предел измерения не менее 200г",
-            "предел измерения 250 мл (C= 2мл) (мензурка)",
-        )
+        adapter.addElement(Bundledesc("Весы электронные", "предел измерения не менее 200г"))
+        adapter.addElement(Bundledesc("Измерительный цилиндр ", "предел измерения 250 мл (C= 2мл) (мензурка)"))
+        init()
+
+    }
+    private fun init(){
+        binding.apply {
+            rcView.layoutManager = LinearLayoutManager(this@L_microMain)
+            rcView.adapter = adapter
+        }
     }
 }
