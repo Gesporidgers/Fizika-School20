7d244afbaacc250ead12142752bfcdbed20eccfc
Gesporidgers <konkord302@gmail.com>
2022-02-04 10:42:00 +0300

    New pages

---
+++ app/src/main/java/com/teovr/fizikaschool20mobile/L_microMain.kt
@@ -0,0 +1,11 @@
+package com.teovr.fizikaschool20mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class L_microMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lmicro_main)
+    }
+}
